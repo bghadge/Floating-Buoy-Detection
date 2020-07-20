5de16a664e348f028c6eda8110285f6e5ebd15d0
Bhushan_ghadge <bghadge@umich.edu>
2020-07-19 23:38:44 -0400

    Created various classes of layers

---
+++ src/model/yolov3/layers.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+
+
+class RouteLayer(nn.Module):
+    def __init__(self, layers):
+        super(RouteLayer, self).__init__()
+        self.layers = layers  # layer indices
+        self.multiple = len(layers) > 1  # multiple layers flag
+
+    def forward(self, x, outputs):
+        if self.multiple:
+            return torch.cat([outputs[i] for i in self.layers], 1)
+        else:
+            return outputs[self.layers[0]]
+
+
+class ShortcutLayer(nn.Module):
+    def __init__(self, layer):
+        super(ShortcutLayer, self).__init__()
+        self.layer = layer  # layer indices
+        # self.n = len(layers) + 1  # number of layers
+
+    def forward(self, x, outputs):
+        return x + outputs[self.layer]
+
+
+class YOLOLayer(nn.Module):
+    def __init__(self, anchors, nc, img_size, yolo_index, stride):
+        super(YOLOLayer, self).__init__()
+        self.anchors = torch.Tensor(anchors)
+        self.index = yolo_index  # index of this layer in layers
+        # self.layers = layers  # model output layer indices
+        self.stride = stride  # layer stride
+        # self.nl = len(layers)  # number of output layers (3)
+        self.na = len(anchors)  # number of anchors (3)
+        self.nc = nc  # number of classes (80)
+        self.no = nc + 5  # number of outputs (85)
+        self.nx, self.ny, self.ng = 0, 0, 0  # initialize no of x, y gridpoints
+        self.anchor_vec = self.anchors / self.stride
+        self.anchor_wh = self.anchor_vec.view(1, self.na, 1, 1, 2)
+
+    def create_grids(self, ng=(13, 13), device='cpu'):
+        self.nx, self.ny = ng  # x and y grid size
+        self.ng = torch.tensor(ng, dtype=torch.float)
+
+        # build xy offsets
+        if not self.training:
+            yv, xv = torch.meshgrid([torch.arange(self.ny, device=device), torch.arange(self.nx, device=device)])
+            self.grid = torch.stack((xv, yv), 2).view((1, 1, self.ny, self.nx, 2)).float()
+
+        if self.anchor_vec.device != device:
+            self.anchor_vec = self.anchor_vec.to(device)
+            self.anchor_wh = self.anchor_wh.to(device)
+
+    def forward(self, p, out):
+        bs, _, ny, nx = p.shape  # bs, 255, 13, 13
+        if (self.nx, self.ny) != (nx, ny):
+            self.create_grids((nx, ny), p.device)
+
+        # p.view(bs, 255, 13, 13) -- > (bs, 3, 13, 13, 85)  # (bs, anchors, grid, grid, classes + xywh)
+        p = p.view(bs, self.na, self.no, self.ny, self.nx).permute(0, 1, 3, 4, 2).contiguous()  # prediction
+
+        if self.training:
+            return p
+        else:  # inference
+            io = p.clone()  # inference output
+            io[..., :2] = torch.sigmoid(io[..., :2]) + self.grid  # xy
+            io[..., 2:4] = torch.exp(io[..., 2:4]) * self.anchor_wh  # wh yolo method
+            io[..., :4] *= self.stride
+            torch.sigmoid_(io[..., 4:])
+            return io.view(bs, -1, self.no), p  # view [1, 3, 13, 13, 85] as [1, 507, 85]
